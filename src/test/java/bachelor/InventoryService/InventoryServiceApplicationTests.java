1e4c6efa80a9a269b5f75f0471a730f8b3a436cc
Mihajlo Mitić <mihajlomiticmiki@gmail.com>
2022-05-18 17:07:33 +0200

    initial project

---
+++ src/test/java/bachelor/InventoryService/InventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package bachelor.InventoryService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
